7c03a4466b7e5ef0f1ecc31764061791dde0b748
Cristian Restituyo <cristian.restituyo@sdq.theoreminc.net>
2016-11-11 11:12:18 -0500

    Added Mole and Stage Object Models.

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,9 +1,24 @@
 var WHACKAMOLE = {};
 
-WHACKAMOLE.MODELS ={
-    Mole:function(){
+WHACKAMOLE.MODELS = {
+    Mole: function(xVal, yVal,speedVal,pointedElement) {
         var self = this;
+        var Position = {
+            x = xVal || 0,
+            y = yVal || 0
+        };
+        var speed = speedVal || 1;
+        var pointer = pointedElement;
+        var states = ['normal','hitted'];
+        var currentState = states[0];
+
+    },
+    Stage:function(pointedElement,speedVal){
+        var self = this;
+        var pointer = pointedElement;
+        var speed = speedVal || 1;
+        var score = 0;
         
     }
-};
 
+};
