57b40152d7f1398f759e5fc15ac2cf96d2abdf4c
1526853 <c.a.restituyonunez@wlv.ac.uk>
2016-11-12 16:34:11 +0000

    Edited gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,27 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    concat = require('gulp-concat')
-;
+    concat = require('gulp-concat');
 
-function errorLog()
-{
+function errorLog() {
     console.error(e);
 }
-gulp.task('watch',function(){
-    gulp.watch('app/assets/sass/**/*.scss',['sass']);
-    gulp.watch('app/assets/js/**/*.js',['scripts']);
+gulp.task('watch', function() {
+    gulp.watch('app/assets/sass/**/*.scss', ['sass']);
+    gulp.watch('app/assets/js/**/*.js', ['scripts']);
 });
 
-gulp.task('sass',function(){
+gulp.task('sass', function() {
     return gulp.src('app/assets/sass/**/*.scss')
-    .pipe(sass())
-    .on('error',errorLog)
-    .pipe(gulp.dest('app/assets/css'))
+        .pipe(sass())
+        .on('error', errorLog)
+        .pipe(gulp.dest('app/assets/css'))
 });
 
-gulp.task('scripts',function(){
+gulp.task('scripts', function() {
     return gulp.src('app/assets/js/**/*.{json,js}')
-          .pipe(concat('bundle.js'))
-          .on('error',errorLog)
-          .pipe(gulp.dest('app/assets/bundle.js'));
+        .pipe(concat('bundle.js'))
+        .on('error', errorLog)
+        .pipe(gulp.dest('app/assets/js'));
 });
 
-gulp.task('default',['sass','scripts','watch']);
+gulp.task('default', ['sass', 'scripts', 'watch']);
